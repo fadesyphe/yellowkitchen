6721dc5cafe59de4ee66f165d78af64e196d40a8
fadesyphe <pivkin.jura2017@gmail.com>
2024-09-25 22:43:09 +0600

    menu logic implemented

--- app.js
+++ app.js
@@ -20,3 +20,36 @@ const swiperSpecialities = new Swiper('.specialities-swiper', {
   },
 });
 
+const buttonMenu = document.querySelector("#button-menu");
+const headerOverlay = document.querySelector(".header-overlay");
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
+
+buttonMenu.addEventListener('click', () => {
+  headerOverlay.classList.toggle('active');
+  menu.classList.toggle('active');
+  buttonMenu.classList.toggle('active');
+  body.classList.toggle('active');
+})
+
+const linksMenu = document.querySelectorAll('a[href*="#"]')
+for (let linkMenu of linksMenu) {
+  linkMenu.addEventListener('click', (e) => {
+    e.preventDefault()
+    const blockId = linkMenu.getAttribute('href').substring(1)
+    headerOverlay.classList.remove('active');
+    menu.classList.remove('active');
+    buttonMenu.classList.remove('active');
+    body.classList.remove('active');
+    document.getElementById(blockId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+const header = document.querySelector('.header');
+let ofsetY = oldOfsetY = 0;
+window.addEventListener('scroll', () => {
+  console.log(window.scrollTop)
+})
